2612857849d7724b73f3c544598f102a4931a543
smdhar <shoumik_dhar@yahoo.com>
2025-07-18 12:18:10 -0400

    Server built

---
+++ server.js
@@ -0,0 +1,12 @@
+// Here is where we import modules
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello, friend!");
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
