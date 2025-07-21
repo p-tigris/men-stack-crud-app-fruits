02deb30151221b575bbde66811646862c196d548
smdhar <shoumik_dhar@yahoo.com>
2025-07-21 15:38:45 -0400

    Create edit functionality

--- server.js
+++ server.js
@@ -68,6 +68,18 @@ app.get('/fruits/:fruitId/edit', async (req, res) => {
     res.render('fruits/edit.ejs', { fruit: foundFruit });
 })
 
+app.put('/fruits/:fruitId', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body); // updates the fruit in the DB
+
+    res.redirect(`/fruits/${req.params.fruitId}`);
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
